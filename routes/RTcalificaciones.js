45d0726d0b9b7b3013c5ac7912d4f4da905d8870
Gerson Castillo <gocastillo@unah.hn>
2022-04-06 22:49:12 -0600

    comentrio

--- routes/RTcalificaciones.js
+++ routes/RTcalificaciones.js
@@ -2,5 +2,5 @@ const Express = require("express");
 const unacalificacion = require("../controller/calificacionesCT.js");
 const router = Express.Router();
 router.get("/buscaCalificacion/:idCalificacion", unacalificacion)
-
+//comentario
 module.exports = router;
