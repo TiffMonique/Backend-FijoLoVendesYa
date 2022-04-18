622f202ff383fde3c28ae323dade9a4798aa6e4e
TiffMonique <tiffanymatamoros72@gmail.com>
2022-04-18 10:29:53 -0600

    Denuncias

--- models/denunciaMD.js
+++ models/denunciaMD.js
@@ -1,21 +1,20 @@
-const db = require('../database/db.js');
-const Sequelize = require('sequelize');
+const db = require("../database/db.js");
+const Sequelize = require("sequelize");
 const { DataTypes } = require("sequelize");
-const modeloUsuarios = require('./UsuariosMD.js');
+const modeloUsuarios = require("./UsuariosMD.js");
 
-const denunciaMD = db.define('Denuncias', {
-    idDenuncia: {type: DataTypes.BIGINT, primaryKey: true, autoIncrement: true},
-    idUsuario: { type: DataTypes.INTEGER, foreignKey: true},
-    motivo:{type: DataTypes.STRING},
-    contenido: {type: DataTypes.STRING},
-    denunciado: { type: DataTypes.INTEGER, foreignKey: true}
-})
+const denunciaMD = db.define("Denuncias", {
+  idDenuncia: { type: DataTypes.BIGINT, primaryKey: true, autoIncrement: true },
+  idUsuario: { type: DataTypes.INTEGER, foreignKey: true },
+  motivo: { type: DataTypes.STRING },
+  contenido: { type: DataTypes.STRING },
+  denunciado: { type: DataTypes.INTEGER, foreignKey: true },
+});
 
+denunciaMD.belongsTo(modeloUsuarios, { foreignKey: "idUsuario" });
+modeloUsuarios.hasMany(denunciaMD, { foreignKey: "idUsuario" });
 
-denunciaMD.belongsTo(modeloUsuarios,{foreignKey:'idUsuario'})
-modeloUsuarios.hasMany(denunciaMD,{foreignKey:'idUsuario'})
-
-denunciaMD.belongsTo(modeloUsuarios,{foreignKey:'idUsuario'})
-modeloUsuarios.hasMany(denunciaMD,{foreignKey:'denunciado'})
+denunciaMD.belongsTo(modeloUsuarios, { foreignKey: "idUsuario" });
+modeloUsuarios.hasMany(denunciaMD, { foreignKey: "denunciado" });
 
 module.exports = denunciaMD;
